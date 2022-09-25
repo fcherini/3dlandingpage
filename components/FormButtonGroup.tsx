a3f243591810863b21198f96e34f2ebb115de513
fcherini <57943717+fcherini@users.noreply.github.com>
2022-09-25 13:50:32 -0300

    fixing flex for btns

--- components/FormButtonGroup.tsx
+++ components/FormButtonGroup.tsx
@@ -5,16 +5,16 @@ export default function FormButtonGroup() {
         <span className="label-text text-14">Idade do aluno</span>
       </label>
       <div className="flex gap-2 w-full">
-        <button className="text-primary-content p-1 w-full btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
+        <button className="text-primary-content p-1 grow btn-sm btn btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
           6 ou menos
         </button>
-        <button className="text-primary-content p-1 w-full btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
+        <button className="text-primary-content p-1 grow btn-sm btn btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
           7 a 10 anos
         </button>
-        <button className="text-primary-content p-1 w-full btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
+        <button className="text-primary-content p-1 grow btn-sm btn btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
           11 a 14 anos
         </button>
-        <button className="text-primary-content p-1 w-full btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
+        <button className="text-primary-content p-1 grow btn-sm btn btn-primary font-poppins font-600 text-14 hover:bg-primary-400">
           14 a 17 anos
         </button>
       </div>
